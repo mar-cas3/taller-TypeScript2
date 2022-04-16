f5d8207678d9d806830a2ff9fbdd2708ae694488
mar-cas3 <61592394+mar-cas3@users.noreply.github.com>
2022-04-16 12:02:30 -0500

    finished - imgs working

--- main.ts
+++ main.ts
@@ -3,19 +3,29 @@ import { series } from "./data.js";
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
 let seriesCaption: HTMLElement = document.getElementById("series-caption")!;
+let seriesDetail: HTMLElement = document.getElementById("tv-card")!;
+seriesDetail.style.visibility = "hidden";
+
+console.log(seriesDetail);
 
 showSeriesTable();
 showStatNumSeason();
+
 
 function showSeriesTable(): void {
     let seriesBody: HTMLElement = document.createElement("tBody");
     for (let serie of series) {
         let tElement: HTMLElement = document.createElement("tr");
-        tElement.innerHTML = `<td style="font-style:bold !important">${serie.id}</td>
-        <td><a href=${serie.website}> ${serie.name}</a></td>
+        tElement.innerHTML = `<td>${serie.id}</td>
+        <td class="series-name">${serie.name}</td>
         <td>${serie.channel}</td>
         <td>${serie.seasons}</td>`;
         seriesBody.appendChild(tElement);
+
+        tElement.getElementsByClassName("series-name")[0].addEventListener("click", () => {
+            showSerieDetail(serie);
+        });
+
     }
     seriesTable.appendChild(seriesBody);
 }
@@ -31,4 +41,26 @@ function showStatNumSeason(): void {
 
     seriesCaption.innerHTML = `Seasons average: ${avgSeason}`;
 }
+
+function showSerieDetail(serie: Serie): void {
+    let seriesCard: HTMLElement = document.getElementById("card-img")!;
+    let seriesTitle: HTMLElement = document.getElementById("card-title")!;
+    let seriesInfo: HTMLElement = document.getElementById("card-text")!;
+    let seriesLink: HTMLElement = document.getElementById("card-link")!;
+    
+    seriesCard.setAttribute("src", serie.image);
+    seriesCard.setAttribute("alt", `${serie.name}'s image`);
+    seriesTitle.innerHTML = serie.name;
+    seriesInfo.innerHTML = `${serie.description}`;
+    seriesLink.innerHTML = `${serie.website}`;
+
+
+    seriesDetail.style.visibility = "visible";
+
+    console.log(seriesCard);
+    console.log(seriesTitle);
+    console.log(seriesInfo);
+
+}
+
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,24 @@
 import { series } from "./data.js";
 var seriesTable = document.getElementById("series");
 var seriesCaption = document.getElementById("series-caption");
+var seriesDetail = document.getElementById("tv-card");
+seriesDetail.style.visibility = "hidden";
+console.log(seriesDetail);
 showSeriesTable();
 showStatNumSeason();
 function showSeriesTable() {
     var seriesBody = document.createElement("tBody");
+    var _loop_1 = function (serie) {
+        var tElement = document.createElement("tr");
+        tElement.innerHTML = "<td>".concat(serie.id, "</td>\n        <td class=\"series-name\">").concat(serie.name, "</td>\n        <td>").concat(serie.channel, "</td>\n        <td>").concat(serie.seasons, "</td>");
+        seriesBody.appendChild(tElement);
+        tElement.getElementsByClassName("series-name")[0].addEventListener("click", function () {
+            showSerieDetail(serie);
+        });
+    };
     for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
         var serie = series_1[_i];
-        var tElement = document.createElement("tr");
-        tElement.innerHTML = "<td style=\"font-style:bold !important\">".concat(serie.id, "</td>\n        <td><a href=").concat(serie.website, "> ").concat(serie.name, "</a></td>\n        <td>").concat(serie.channel, "</td>\n        <td>").concat(serie.seasons, "</td>");
-        seriesBody.appendChild(tElement);
+        _loop_1(serie);
     }
     seriesTable.appendChild(seriesBody);
 }
@@ -23,4 +32,19 @@ function showStatNumSeason() {
     }
     var avgSeason = totalSeason / totalSeries;
     seriesCaption.innerHTML = "Seasons average: ".concat(avgSeason);
+}
+function showSerieDetail(serie) {
+    var seriesCard = document.getElementById("card-img");
+    var seriesTitle = document.getElementById("card-title");
+    var seriesInfo = document.getElementById("card-text");
+    var seriesLink = document.getElementById("card-link");
+    seriesCard.setAttribute("src", serie.image);
+    seriesCard.setAttribute("alt", "".concat(serie.name, "'s image"));
+    seriesTitle.innerHTML = serie.name;
+    seriesInfo.innerHTML = "".concat(serie.description);
+    seriesLink.innerHTML = "".concat(serie.website);
+    seriesDetail.style.visibility = "visible";
+    console.log(seriesCard);
+    console.log(seriesTitle);
+    console.log(seriesInfo);
 }
